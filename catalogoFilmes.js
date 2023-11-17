544694bde5c761dd64b018a51f0584e3a58c38aa
maths9 <devilcaos110@gmail.com>
2023-11-17 01:01:49 -0300

    #	new file:   catalogoFilmes.html #	new file:   catalogoFilmes.js #	new file:  catalogoFilmes/aula03.css

---
+++ catalogoFilmes.js
@@ -0,0 +1,9 @@
+var listaFilmes = ['https://upload.wikimedia.org/wikipedia/pt/thumb/1/13/John_wick_ver3.jpg/250px-John_wick_ver3.jpg',
+ 'https://upload.wikimedia.org/wikipedia/pt/thumb/1/13/John_wick_ver3.jpg/250px-John_wick_ver3.jpg', 
+ 'https://upload.wikimedia.org/wikipedia/pt/thumb/1/13/John_wick_ver3.jpg/250px-John_wick_ver3.jpg'] 
+
+ 
+for( var i = 0; i < listaFilmes.length;  i++){
+document.write('<img src=' +  listaFilmes[i] + '>')
+
+}
